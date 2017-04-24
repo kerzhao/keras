17403981e26406bd62abd47d375e96b353560923
justttry <kerzhao@163.com>
2017-04-24 12:48:57 +0800

    add captcha_break_test.py

---
+++ examples/captcha_break_test.py
@@ -0,0 +1,55 @@
+from captcha.image import ImageCaptcha
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+import string
+characters = string.digits + string.ascii_uppercase
+print(characters)
+
+width, height, n_len, n_class = 170, 80, 4, len(characters)
+
+from keras.utils.np_utils import to_categorical
+
+def gen(batch_size=32):
+    X = np.zeros((batch_size, height, width, 3), dtype=np.uint8)
+    y = [np.zeros((batch_size, n_class), dtype=np.uint8) for i in range(n_len)]
+    generator = ImageCaptcha(width=width, height=height)
+    while True:
+        for i in range(batch_size):
+            random_str = ''.join([random.choice(characters) for j in range(4)])
+            X[i] = generator.generate_image(random_str)
+            for j, ch in enumerate(random_str):
+                y[j][i, :] = 0
+                y[j][i, characters.find(ch)] = 1
+        yield X, y
+        
+def decode(y):
+    y = np.argmax(np.array(y), axis=2)[:,0]
+    return ''.join([characters[x] for x in y])
+
+X, y = next(gen(1))
+plt.imshow(X[0])
+plt.title(decode(y))
+
+from keras.models import *
+from keras.layers import *
+
+input_tensor = Input((height, width, 3))
+x = input_tensor
+for i in range(4):
+    x = Convolution2D(32*2**i, 3, 3, activation='relu')(x)
+    x = Convolution2D(32*2**i, 3, 3, activation='relu')(x)
+    x = MaxPooling2D((2, 2))(x)
+
+x = Flatten()(x)
+x = Dropout(0.25)(x)
+x = [Dense(n_class, activation='softmax', name='c%d'%(i+1))(x) for i in range(4)]
+model = Model(input=input_tensor, output=x)
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adadelta',
+              metrics=['accuracy'])
+
+model.fit_generator(gen(), samples_per_epoch=51200, nb_epoch=5,
+                    validation_data=gen(), nb_val_samples=1280)
